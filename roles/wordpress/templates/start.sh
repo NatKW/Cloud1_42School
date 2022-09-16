a2b4b9f5707b5000b546360bfc2965472063d216
NatKW <nath.burgade@gmail.com>
2022-09-16 19:57:48 +0200

    sept16

--- roles/wordpress/templates/start.sh
+++ roles/wordpress/templates/start.sh
@@ -12,8 +12,6 @@ if [ ! -f "/var/www/html/index.php" ]; then
     wp config create --dbname=$MARIADB_DATABASE --dbuser=$MARIADB_USER --dbpass=$MARIADB_PASSWORD --dbhost=$WORDPRESS_DB_HOST --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
     wp core install --url=$DOMAIN --title=$WORDPRESS_DB_TITLE --admin_user=$WORDPRESS_DB_ADMIN --admin_password=$WORDPRESS_DB_ADMIN_PWD --admin_email=$WORDPRESS_DB_ADMIN_EMAIL --allow-root
     wp user create $WP_USER $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
-    wp theme install  twenty sixteen --activate --allow-root
-    wp theme update twenty sixteen --allow-root
     wp plugin update --all --allow-root
 
 fi
